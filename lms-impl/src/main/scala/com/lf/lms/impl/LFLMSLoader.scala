42e550902429dc0cf1cc5bc3c408c2c69895d378
yash-lf <yashpal.bhardwaj@loanframe.com>
2018-12-07 14:01:29 +0530

    Deleted Unwanted files

--- lms-impl/src/main/scala/com/lf/lms/impl/LFLMSLoader.scala
+++ lms-impl/src/main/scala/com/lf/lms/impl/LFLMSLoader.scala
@@ -4,9 +4,7 @@ import com.lf.lms.api.{LFLMSService, LMSService}
 import com.lightbend.lagom.scaladsl.api.ServiceLocator
 import com.lightbend.lagom.scaladsl.api.ServiceLocator.NoServiceLocator
 import com.lightbend.lagom.scaladsl.devmode.LagomDevModeComponents
-import com.lightbend.lagom.scaladsl.persistence.ReadSidePersistenceComponents
 import com.lightbend.lagom.scaladsl.server._
-import com.loanframe.lfdb.connection.PSQLComponent
 import com.softwaremill.macwire._
 import play.api.libs.ws.ahc.AhcWSComponents
 
@@ -21,7 +19,7 @@ class LFAppLoader extends LagomApplicationLoader {
   override def describeService = Some(readDescriptor[LFLMSService])
 }
 
-abstract class LFApp(context: LagomApplicationContext) extends LagomApplication(context) with ReadSidePersistenceComponents with AhcWSComponents {
+abstract class LFApp(context: LagomApplicationContext) extends LagomApplication(context) with AhcWSComponents {
 
   // Bind the service that this server provides
   override lazy val lagomServer = serverFor[LFLMSService](wire[LFLMSServiceImpl])
--- lms-impl/src/main/scala/com/lf/lms/impl/PersitenceEntityLMS.scala
+++ lms-impl/src/main/scala/com/lf/lms/impl/PersitenceEntityLMS.scala
@@ -2,4 +2,7 @@ package com.lf.lms.impl
 
 import com.loanframe.lfdb.models._
 
-class PersistenceEntityLMS(loginTable: LoginTable) {}
+class PersistenceEntityLMS(loginTable: LoginTable) {
+
+
+}
